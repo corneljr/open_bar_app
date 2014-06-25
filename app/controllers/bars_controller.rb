9b4b44ac109a333febd0799e0a89e80626836b18
gnazarkin <gleb.nazarkin@gmail.com>
2014-06-25 15:10:30 -0400

    Changed up plural issues with files

--- app/controllers/bars_controller.rb
+++ app/controllers/bars_controller.rb
@@ -1,22 +1,47 @@
 class BarsController < ApplicationController
-  def index
+
+ def show
+    @bar = Bar.find([params[:id])
   end
 
   def new
-  end
-
-  def create
-  end
-
-  def show
+    @bar = Bar.new
   end
 
   def edit
+    @bar = Bar.find([params[:id])
+  end
+
+  def create
+    @bar = Bar.new(bar_params)
+
+    if @bar.save
+      redirect_to bars_url
+    else
+      render :new
+    end
+
   end
 
   def update
+    @bar = Bar.find(params[:id])
+
+    if @bar.update_attributes(bar_params)
+      redirect_to bars_path(@bar)
+    else 
+      render :edit
+    end 
   end
 
   def destroy
+    @bar = Bar.find(params[:id])
+    @bar.destroy
+    rediret_to bars_path
   end
+
+  private
+  def bar_params
+    params.require(:bar).permit(:name, :address, :latitute, :longitude, :capacity, :description, :user_id, :category)
+  end
+
 end
